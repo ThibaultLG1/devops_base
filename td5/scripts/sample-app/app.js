e5d9aa796cd9e4926c39f7089a92dc8cb168de0d
thibault le guidevais <thibault.leguidevais@gmail.com>
2025-02-07 23:16:02 +0000

    Introduce intentional error

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
+  res.send('DevOps Labs!');
 });
 
 const port = process.env.JEST_WORKER_ID ? 0 : process.env.PORT || 8080;
